4f197ad36183e61d60cc404520fd7b4a3abec006
Silvostepa-dlss <octaviangogailie@outlook.com>
2026-05-23 20:20:07 +0300

    Update savingsAcc.java

--- src/Exceptions/Main.java
+++ src/Exceptions/Main.java
@@ -1,14 +1,34 @@
 package Exceptions;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BankAcc cont = new BankAcc(20.0F, "Marian");
+        System.out.println("Cont creat pentru " + cont.getOwner() + " cu suma: " + cont.getAmount());
 
         try {
             cont.deposit(10);
-            cont.withdraw(10);
+            System.out.println("După depunere: " + cont.getAmount());
+
+            System.out.println("Se încearcă retragerea sumei de 50...");
+            cont.withdraw(50);
+
         } catch (InsufficientFundsException | InvalidAmountException e) {
-            System.out.println(((Exception)e).getMessage());
+            System.out.println("Excepție prinsă: " + e.getMessage());
+        }
+
+        System.out.println();
+
+        SavingsAcc contEconomii = new SavingsAcc(100.0f, "Andrei", 0.10f);
+        System.out.println("Cont economii creat pentru " + contEconomii.getOwner() + " cu suma: " + contEconomii.getAmount());
+
+        contEconomii.applyInterest();
+        System.out.println("Sold după aplicare dobândă: " + contEconomii.getAmount());
+
+        try {
+            System.out.println("Se încearcă depunerea sumei de -5...");
+            contEconomii.deposit(-5);
+        } catch (InvalidAmountException e) {
+            System.out.println("Excepție prinsă: " + e.getMessage());
         }
     }
 }
--- src/Exceptions/SavingsAcc.java
+++ src/Exceptions/SavingsAcc.java
@@ -1,6 +1,6 @@
 package Exceptions;
 
-public class SavingsAcc extends BankAcc{
+public class SavingsAcc extends BankAcc {
     private float interestRate;
 
     public SavingsAcc() {
@@ -10,5 +10,15 @@ public class SavingsAcc extends BankAcc{
     public SavingsAcc(float amount, String owner, float interestRate) {
         super(amount, owner);
         this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        float interest = getAmount() * interestRate;
+        try {
+            deposit((int) interest);
+            System.out.println("Dobânda de " + interest + " a fost aplicată.");
+        } catch (InvalidAmountException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
